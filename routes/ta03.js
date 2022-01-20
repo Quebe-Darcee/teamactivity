6147ea737414c15f733e00f567d3cc74d896651b
Darcee Quebe <darceerl@byui.edu>
2022-01-19 17:04:32 -0700

    Add ta03 assignment

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,6 +1,15 @@
-//TA03 PLACEHOLDER
+//TA03
 const express = require('express');
 const router = express.Router();
+
+
+let items = "";
+const request = require("request");
+request('https://byui-cse.github.io/cse341-course/lesson03/items.json', (error, response, body) => {
+  items = JSON.parse(body);
+});
+
+console.log(items);
 
 router.get('/', (req, res, next) => {
   res.render('pages/ta03', {
@@ -8,7 +17,24 @@ router.get('/', (req, res, next) => {
     path: '/ta03', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    items: items
   });
 });
+
+
+router.post('/search', (req, res, next) => {
+  const searchText = req.body.searchText;
+
+  const result = Object.values(items).filter(word => word.name === searchText);
+  console.log(result)
+
+  res.render('pages/ta03', {
+    title: 'Team Activity 03',
+    path: '/ta03', // For pug, EJS
+    activeTA03: true, // For HBS
+    contentCSS: true, // For HBS
+    items: result
+  });
+})
 
 module.exports = router;
